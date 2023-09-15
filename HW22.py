a92ae5d5717a8c285d076e430e552f10e625b91a
SAN DIN <yan_san_din@mail.ru>
2023-09-16 03:00:36 +0900

    first commit

---
+++ HW22.py
@@ -0,0 +1,16 @@
+print("Введите первый массив: ")
+a= list(map(int,input().split()))
+print(a)
+print("Введите второй массив: ")
+b= list(map(int,input().split()))
+print(b)
+c=[]
+for i in (a) and (b):
+    if i in a and b:
+        c.append(i)
+
+c.sort()
+print(c)
+
+        
+
